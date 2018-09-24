58963fc43133e12641e96d47f90ecff2730310cc
fauszt <fausztusz02@gmail.com>
2018-09-24 15:57:04 +0200

    verzio 1.0

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2 {
     class Program {
+        
+
         static void Main(string[] args) {
+
+            Console.WriteLine("Add meg a szamok mennyiseget");
+            int nSzam = Convert.ToInt32(Console.ReadLine());
+            int[] t = new int[nSzam];
+            for (int i = 0; i < nSzam; i++) {
+                Console.WriteLine("Add meg a " + (i + 1) + " szamot");
+                t[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.ReadLine();
+
+
+
 
         }
     }
---
+++ ConsoleApplication2/Scanner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication2 {
+    internal class Scanner {
+        internal int nextInt() {
+            throw new NotImplementedException();
+        }
+
+        internal void Close() {
+            throw new NotImplementedException();
+        }
+    }
+}
